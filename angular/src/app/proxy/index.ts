2bf27c6e410103485016b1f84b001ae870ed29b5
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2024-03-06 21:17:45 +0700

    proxy

---
+++ angular/src/app/proxy/index.ts
@@ -0,0 +1,5 @@
+import * as Ntkecommerce from './ntkecommerce';
+import * as ProductCategories from './product-categories';
+import * as Products from './products';
+export * from './models';
+export { Ntkecommerce, ProductCategories, Products };
